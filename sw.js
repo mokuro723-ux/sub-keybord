89a4c59d91874cee4d0b59e64ff98130dd9a680b
mokuro723-ux <mokuro723@gmail.com>
2026-06-15 10:58:18 +0900

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("v1").then(c => c.addAll(["lefthand-device-config.html", "manifest.json", "knob-192.png", "knob-512.png"]))
-  );
-});
-self.addEventListener("fetch", e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-});
